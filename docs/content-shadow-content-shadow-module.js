dcc8f93734f4b58e65def95cf983dd9a8ecf5eae
hbxyxuxiaodong <1334339809@163.com>
2019-01-02 11:21:33 +0800

    add

---
+++ docs/content-shadow-content-shadow-module.js
@@ -0,0 +1,305 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["content-shadow-content-shadow-module"],{
+
+/***/ "./src/app/content-shadow/child-three/child-three.component.html":
+/*!***********************************************************************!*\
+  !*** ./src/app/content-shadow/child-three/child-three.component.html ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"panel\">\n  <div class=\"panel-heading\">\n    这里是被投影的自定义组件\n  </div>\n  <div class=\"panel-body\">\n     <button>sayhello</button>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/child-three/child-three.component.scss":
+/*!***********************************************************************!*\
+  !*** ./src/app/content-shadow/child-three/child-three.component.scss ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NvbnRlbnQtc2hhZG93L2NoaWxkLXRocmVlL2NoaWxkLXRocmVlLmNvbXBvbmVudC5zY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/child-three/child-three.component.ts":
+/*!*********************************************************************!*\
+  !*** ./src/app/content-shadow/child-three/child-three.component.ts ***!
+  \*********************************************************************/
+/*! exports provided: ChildThreeComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ChildThreeComponent", function() { return ChildThreeComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var ChildThreeComponent = /** @class */ (function () {
+    function ChildThreeComponent() {
+    }
+    ChildThreeComponent.prototype.ngOnInit = function () {
+    };
+    ChildThreeComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-child-three',
+            template: __webpack_require__(/*! ./child-three.component.html */ "./src/app/content-shadow/child-three/child-three.component.html"),
+            styles: [__webpack_require__(/*! ./child-three.component.scss */ "./src/app/content-shadow/child-three/child-three.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], ChildThreeComponent);
+    return ChildThreeComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/child-two/child-two.component.html":
+/*!*******************************************************************!*\
+  !*** ./src/app/content-shadow/child-two/child-two.component.html ***!
+  \*******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"child\">\n    <div class=\"header\">\n      <ng-content select=\"h3\"></ng-content>\n    </div>\n    <!--<div class=\"body\">-->\n      <!--<ng-content select=\"app-child-three\"></ng-content>-->\n    <!--</div>-->\n    <div class=\"footer\">\n      <ng-content select=\"p\"></ng-content>\n    </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/child-two/child-two.component.scss":
+/*!*******************************************************************!*\
+  !*** ./src/app/content-shadow/child-two/child-two.component.scss ***!
+  \*******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".child {\n  background-color: yellow; }\n\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi93ZWJwcm9qZWN0L2FuZ3VsYXIvdHlodC9zcmMvYXBwL2NvbnRlbnQtc2hhZG93L2NoaWxkLXR3by9jaGlsZC10d28uY29tcG9uZW50LnNjc3MiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFDRSx5QkFBd0IsRUFDekIiLCJmaWxlIjoic3JjL2FwcC9jb250ZW50LXNoYWRvdy9jaGlsZC10d28vY2hpbGQtdHdvLmNvbXBvbmVudC5zY3NzIiwic291cmNlc0NvbnRlbnQiOlsiLmNoaWxke1xuICBiYWNrZ3JvdW5kLWNvbG9yOiB5ZWxsb3c7XG59XG4iXX0= */"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/child-two/child-two.component.ts":
+/*!*****************************************************************!*\
+  !*** ./src/app/content-shadow/child-two/child-two.component.ts ***!
+  \*****************************************************************/
+/*! exports provided: ChildTwoComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ChildTwoComponent", function() { return ChildTwoComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var ChildTwoComponent = /** @class */ (function () {
+    function ChildTwoComponent() {
+    }
+    ChildTwoComponent.prototype.ngOnInit = function () {
+    };
+    ChildTwoComponent.prototype.ngAfterContentInit = function () {
+        // console.log('子组件投影内容被加载完成')
+    };
+    ChildTwoComponent.prototype.ngAfterContentChecked = function () {
+        // console.log('子组件投影内容变更检测完成')
+    };
+    ChildTwoComponent.prototype.ngAfterViewInit = function () {
+        // console.log('子组件视图加载完成')
+    };
+    ChildTwoComponent.prototype.ngAfterViewChecked = function () {
+        // console.log('子组件视图变更检测完成')
+    };
+    ChildTwoComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-child-two',
+            template: __webpack_require__(/*! ./child-two.component.html */ "./src/app/content-shadow/child-two/child-two.component.html"),
+            styles: [__webpack_require__(/*! ./child-two.component.scss */ "./src/app/content-shadow/child-two/child-two.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], ChildTwoComponent);
+    return ChildTwoComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/content-shadow-routing.ts":
+/*!**********************************************************!*\
+  !*** ./src/app/content-shadow/content-shadow-routing.ts ***!
+  \**********************************************************/
+/*! exports provided: ContentShadowRouting */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ContentShadowRouting", function() { return ContentShadowRouting; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ng_content_ng_content_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ng-content/ng-content.component */ "./src/app/content-shadow/ng-content/ng-content.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var route = [{
+        path: '', component: _ng_content_ng_content_component__WEBPACK_IMPORTED_MODULE_2__["NgContentComponent"]
+    }];
+var ContentShadowRouting = /** @class */ (function () {
+    function ContentShadowRouting() {
+    }
+    ContentShadowRouting = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(route)],
+            exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]]
+        })
+    ], ContentShadowRouting);
+    return ContentShadowRouting;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/content-shadow.module.ts":
+/*!*********************************************************!*\
+  !*** ./src/app/content-shadow/content-shadow.module.ts ***!
+  \*********************************************************/
+/*! exports provided: ContentShadowModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ContentShadowModule", function() { return ContentShadowModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _share_share_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../share/share.module */ "./src/app/share/share.module.ts");
+/* harmony import */ var _ng_content_ng_content_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ng-content/ng-content.component */ "./src/app/content-shadow/ng-content/ng-content.component.ts");
+/* harmony import */ var _child_two_child_two_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./child-two/child-two.component */ "./src/app/content-shadow/child-two/child-two.component.ts");
+/* harmony import */ var _child_three_child_three_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./child-three/child-three.component */ "./src/app/content-shadow/child-three/child-three.component.ts");
+/* harmony import */ var _content_shadow_routing__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./content-shadow-routing */ "./src/app/content-shadow/content-shadow-routing.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var ContentShadowModule = /** @class */ (function () {
+    function ContentShadowModule() {
+    }
+    ContentShadowModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            imports: [
+                _share_share_module__WEBPACK_IMPORTED_MODULE_1__["ShareModule"],
+                _content_shadow_routing__WEBPACK_IMPORTED_MODULE_5__["ContentShadowRouting"]
+            ],
+            declarations: [_ng_content_ng_content_component__WEBPACK_IMPORTED_MODULE_2__["NgContentComponent"], _child_two_child_two_component__WEBPACK_IMPORTED_MODULE_3__["ChildTwoComponent"], _child_three_child_three_component__WEBPACK_IMPORTED_MODULE_4__["ChildThreeComponent"]]
+        })
+    ], ContentShadowModule);
+    return ContentShadowModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/ng-content/ng-content.component.html":
+/*!*********************************************************************!*\
+  !*** ./src/app/content-shadow/ng-content/ng-content.component.html ***!
+  \*********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"parent\">\n   <div class=\"panel-heading\">父组件</div>\n   <div class=\"panel-body\">\n     <app-child-two>\n       <h3>这是父层投影进来的内容</h3>\n       <app-child-three></app-child-three>\n       <p>这是底部内容</p>\n     </app-child-two>\n   </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/ng-content/ng-content.component.scss":
+/*!*********************************************************************!*\
+  !*** ./src/app/content-shadow/ng-content/ng-content.component.scss ***!
+  \*********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".parent {\n  background-color: #00a8ff; }\n\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi93ZWJwcm9qZWN0L2FuZ3VsYXIvdHlodC9zcmMvYXBwL2NvbnRlbnQtc2hhZG93L25nLWNvbnRlbnQvbmctY29udGVudC5jb21wb25lbnQuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLDBCQUF5QixFQUMxQiIsImZpbGUiOiJzcmMvYXBwL2NvbnRlbnQtc2hhZG93L25nLWNvbnRlbnQvbmctY29udGVudC5jb21wb25lbnQuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIi5wYXJlbnR7XG4gIGJhY2tncm91bmQtY29sb3I6ICMwMGE4ZmY7XG59XG4iXX0= */"
+
+/***/ }),
+
+/***/ "./src/app/content-shadow/ng-content/ng-content.component.ts":
+/*!*******************************************************************!*\
+  !*** ./src/app/content-shadow/ng-content/ng-content.component.ts ***!
+  \*******************************************************************/
+/*! exports provided: NgContentComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NgContentComponent", function() { return NgContentComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var NgContentComponent = /** @class */ (function () {
+    function NgContentComponent() {
+    }
+    NgContentComponent.prototype.ngOnInit = function () {
+    };
+    NgContentComponent.prototype.ngAfterContentInit = function () {
+        // console.log('父组件投影内容加载完成');
+    };
+    NgContentComponent.prototype.ngAfterContentChecked = function () {
+        // console.log('父组件投影内容变更检测完毕');
+    };
+    NgContentComponent.prototype.ngAfterViewInit = function () {
+        // console.log('父组件视图加载完成')
+    };
+    NgContentComponent.prototype.ngAfterViewChecked = function () {
+        // console.log('父组件视图变更检测完成')
+    };
+    NgContentComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-ng-content',
+            template: __webpack_require__(/*! ./ng-content.component.html */ "./src/app/content-shadow/ng-content/ng-content.component.html"),
+            styles: [__webpack_require__(/*! ./ng-content.component.scss */ "./src/app/content-shadow/ng-content/ng-content.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], NgContentComponent);
+    return NgContentComponent;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=content-shadow-content-shadow-module.js.map
